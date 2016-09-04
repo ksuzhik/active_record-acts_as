88c60dff4d068b20598c4f01c058ba99fd07d797
Kseniya Timofeeva <ksuzhik@gmail.com>
2016-09-04 21:58:21 +0300

    update to acteverecord 5

--- active_record-acts_as.gemspec
+++ active_record-acts_as.gemspec
@@ -25,6 +25,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec", "~> 3"
   spec.add_development_dependency "rake", "~> 10"
 
-  spec.add_dependency "activesupport", "~> 4"
-  spec.add_dependency "activerecord", "~> 4", ">= 4.1.2"
+  # spec.add_dependency "activesupport", "~> 4"
+  # spec.add_dependency "activerecord", "~> 4", ">= 4.1.2"
+  spec.add_dependency 'activesupport', '>= 4', '<= 5.0.0.rc2'
+  spec.add_dependency 'activerecord', '>= 4.1.2', '<= 5.0.0.rc2'
 end
--- lib/active_record/acts_as/class_methods.rb
+++ lib/active_record/acts_as/class_methods.rb
@@ -1,13 +1,29 @@
 module ActiveRecord
   module ActsAs
-    module ClassMethods
-      def self.included(module_)
-        module_.alias_method_chain :_reflections, :acts_as
+    module ReflectionsWithActAs
+      def _reflections
+        @_reflections_acts_as_cache ||=
+          super.reverse_merge(acting_as_model._reflections)
       end
 
-      def _reflections_with_acts_as
-        @_reflections_acts_as_cache ||=
-          _reflections_without_acts_as.reverse_merge(acting_as_model._reflections)
+      def _reflect_on_association(table_name)
+        case table_name
+        when String then
+          super(table_name) || super(table_name.singularize.to_sym)
+        else
+          super(table_name)
+        end
+      end
+
+      def clear_reflections_cache
+        @_reflections_acts_as_cache = nil
+        super
+      end
+    end
+
+    module ClassMethods
+      def self.included(mod)
+        mod.prepend ReflectionsWithActAs
       end
 
       def validators_on(*args)
